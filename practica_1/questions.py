b846c31b06c9bca2b35ab9bb2f4a0794e61c2e6d
Valentin Volatile <valentin.volatiles@gmail.com>
2025-03-15 23:28:15 -0300

    Refactoreo usando zip

--- practica_1/questions.py
+++ practica_1/questions.py
@@ -23,17 +23,17 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+# lista de tuples que tiene pregunta, respuestas posibles e índice de la respuesta correcta
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 
 score = 0.0
 
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-
+for question in questions_to_ask:
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(question[0])
+    
+    for i, answer in enumerate(question[1]):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
@@ -42,12 +42,12 @@ for _ in range(3):
         
         # Se verifica que la respuesta sea válida
         user_answer = user_answer.strip() #ignoramos trailing o leading spaces
-        if(not user_answer.isnumeric() or int(user_answer) > len(answers[question_index])):
+        if(not user_answer.isnumeric() or int(user_answer) > len(question[1])):
             print("Respuesta no válida")
             exit(1)
         
         # Se verifica si la respuesta es correcta
-        if int(user_answer)-1 == correct_answers_index[question_index]:
+        if int(user_answer)-1 == question[2]:
             print("¡Correcto!")
             score += 1
             break
@@ -57,7 +57,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(question[1][question[2]])
 
     # Se imprime un blanco al final de la pregunta
     print()
